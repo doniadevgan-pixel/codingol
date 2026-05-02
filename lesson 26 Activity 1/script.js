69d1d00e7cf9ce77339d9aa98e6e155cd5f7ccf9
doniadevgan-pixel <doniadevgan@gmail.com>
2026-05-02 16:40:18 +1000

    Co-authored-by: dahmd <dahmd@users.noreply.github.com>

---
+++ lesson 26 Activity 1/script.js
@@ -0,0 +1,10 @@
+
+var ABSOLUTE_VALUE = Math.abs(10) 
+var PI_VALUE = Math.PI
+var random_variable = Math.random()*10
+var Rounded = Math.round(random_variable)
+
+    document.getElementById("mydiv").innerHTML =ABSOLUTE_VALUE ;
+    document.getElementById("mydiv4").innerHTML =random_variable ;
+    document.getElementById("mydiv3").innerHTML =Rounded ;
+   document.getElementById("mydiv2").innerHTML =PI_VALUE ;
---
+++ lesson 27 Activity 1/script.js
@@ -0,0 +1,17 @@
+
+function Check(e){
+
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+
+
+
+
+    alert(name,email,password)
+}
+
+
+
+
+document.getElementById("Mylogin").onsubmit = Check
