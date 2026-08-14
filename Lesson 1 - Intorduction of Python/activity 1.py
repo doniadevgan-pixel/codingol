62580430cac6c0258e53d1d68fc775621cc42ed1
doniadevgan-pixel <doniadevgan@gmail.com>
2026-08-14 17:20:40 +1000

    okay

---
+++ Lesson 1 - Intorduction of Python/activity 1.py
@@ -0,0 +1,4 @@
+print("Hello my name is Mitul Sharma, I am in year nine and currently I am studing at Evelyn Scott School.")
+import keyword
+print (keyword.kwlist)
+# This is my introduction to python programming and I am very excited to learn more about it.
