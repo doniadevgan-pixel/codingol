5805b1c723f62e2394239f8e386cd30a90db0dd8
doniadevgan-pixel <doniadevgan@gmail.com>
2026-09-01 17:51:25 +1000

    hello

---
+++ Lesson 43 - Python - function/Activity 2.py
@@ -0,0 +1,17 @@
+def recursion_factorial(n):
+    if  n==1: 
+         return 1
+    else:
+         return n*recursion_factorial(n-1)
+
+num=int(input("enter a number:"))
+
+# Cheack if the number is negative
+
+if num <0:
+     print("Sorry, factorial does not exist for negative numbers")
+elif num==0:
+    print("the factorial of 0 is 1")
+else:
+    print("the factorial of", num, "is", recursion_factorial(num))
+    
